6a8fc3f099d434ee5f052188594812207b547455
FIAP <FIAP>
2019-11-05 22:18:17 -0300

    Rota teste de Products.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/products', (req, res, next) => {
+    res.json({
+        id: 123,
+        nome: 'Leandro'
+    });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
