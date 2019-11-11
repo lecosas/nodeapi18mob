4bd82104c5c2d11bd3375cd7ebc94039cb3b6e5c
Leandro Cosas <lecosas@gmail.com>
2019-11-11 10:47:25 -0300

    Ajustes na rota de clients.

--- routes/clients.js
+++ routes/clients.js
@@ -7,5 +7,6 @@ const Clients = require('../controllers/Clients');
 router.get('/', Clients.list);
 router.get('/:id', Clients.get);
 router.put('/:id', Clients.update);
+router.delete('/:id', Clients.delete);
 
 module.exports = router;
